9d6a8ef9069bb7f741db8ff7ee39b8d58daeeb8e
Alberto Ramos <bertoramos2001@gmail.com>
2021-10-21 09:26:48 +0200

    version 2 practica

---
+++ TP/src/es/ucm/tp1/supercars/control/commands/InfoCommand.java
@@ -0,0 +1,28 @@
+package es.ucm.tp1.supercars.control.commands;
+
+import es.ucm.tp1.supercars.logic.Game;
+import es.ucm.tp1.supercars.utils.StringUtils;
+import es.ucm.tp1.supercars.view.GamePrinter;
+
+public class InfoCommand extends Command {
+
+	private static final String NAME = "info";
+
+	private static final String DETAILS = "[i]nfo";
+
+	private static final String SHORTCUT = "i";
+
+	private static final String HELP = "prints gameobject info";
+
+	public InfoCommand() {
+		super(NAME, SHORTCUT, DETAILS, HELP);
+	}
+
+	@Override
+	public boolean execute(Game game) {
+		System.out.print("Available objects:");
+		// TODO add your code
+		return false;
+	}
+
+}
---
+++ TP/src/es/ucm/tp1/supercars/control/commands/UpdateCommand.java
@@ -0,0 +1,31 @@
+package es.ucm.tp1.supercars.control.commands;
+
+import es.ucm.tp1.supercars.logic.Game;
+
+public class UpdateCommand extends Command {
+
+	private static final String NAME = "update";
+
+	private static final String DETAILS = "[n]one | []";
+
+	private static final String SHORTCUT = "n";
+
+	private static final String HELP = "update";
+
+	public UpdateCommand() {
+		super(NAME, SHORTCUT, DETAILS, HELP);
+	}
+
+	@Override
+	public boolean execute(Game game) {
+		// TODO add your code
+	}
+
+	@Override
+	protected Command parse(String[] commandWords) {
+		if ("".equalsIgnoreCase(commandWords[0])) {
+			commandWords[0] = SHORTCUT;
+		}
+		return super.parse(commandWords);
+	}
+}
