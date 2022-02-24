d65a69ca7ebd4c0f633bccaa993db874b3cd2bbe
Alberto Ramos Suárez <bertoramos2001@gmail.com>
2022-02-24 10:37:05 +0100

    Update GameSerializer.java
    quitado una task sobrante

--- TP/src/es/ucm/tp1/supercars/view/GameSerializer.java
+++ TP/src/es/ucm/tp1/supercars/view/GameSerializer.java
@@ -21,7 +21,6 @@ public class GameSerializer {
 		s.append(getGameInfo());
 
 		if (!game.testMode()) {
-			// TODO: aqui no se si poner ms, s o que
 			s.append("EllapsedTime: " + (int) (game.getCurrentTime() * 1000) + "\n");
 		}
 
